2ca08a7266517d4f01b5919874126f0f7f9cfa38
CarolinaFaria <carolina.faria@zup.com.br>
2021-08-19 10:52:19 -0300

    setup projeto

---
+++ src/main/java/br/com/zup/edu/desafioproposta/DesafiopropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.desafioproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiopropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiopropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/desafioproposta/DesafiopropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.desafioproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiopropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
